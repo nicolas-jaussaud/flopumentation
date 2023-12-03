3cd4ae82f35d3344f9a1bcc2ac81323bfbd8a8f0
Nicolas Jaussaud <nicolas@teamtangible.com>
2023-12-03 18:35:27 -0500

    Navigation: Keep track of history and load right page on initial loading if location contains hashbang; Add footer navigation

--- src/scripts/Flopumentation.js
+++ src/scripts/Flopumentation.js
@@ -5,13 +5,15 @@ class Flopumentation {
   
   constructor(config) {
 
-    this.page = new Page(config)
     this.config = config
+
+    this.page = new Page(config)
     
     document.addEventListener('DOMContentLoaded', () => {
       this.page.generateDOM()
       this.initMenuListener()
-      this.loadPage(this.config.initialPage ?? '')
+      this.loadInitialPage()
+      this.initHistoryListener()
     })
 
     this.initMenuListener.bind(this)
@@ -19,11 +21,30 @@ class Flopumentation {
     this.loadPage.bind(this)
   }
 
+  loadInitialPage = () => { 
+    
+    if( ! window.location.hash.includes('#!') ) {
+      return this.loadPage(this.config.initialPage ?? '')
+    }
+
+    const initialPage = window.location.hash.split('#!').pop()
+    this.loadPage(initialPage)
+  }
+
+  initHistoryListener = () => {
+    window.addEventListener('popstate', event => {
+      const target = event.target.window.location.hash
+      if( ! target.includes('#!') ) return;
+      const page = target.split('#!').pop()
+      if( page !== this.page.current ) this.loadPage(page)
+    })
+  }
+
   initMenuListener = () => {
     const pages = this.page.getMenuItems()
     pages.forEach(item => {
       item.addEventListener('click', event => {
-        this.loadPage( event.target.getAttribute('href').substring(1) )
+        this.loadPage( event.target.getAttribute('href').substring(2) )
       })
     })
   }
@@ -33,7 +54,7 @@ class Flopumentation {
     links.forEach(link => {
       const href = link.getAttribute('href')
       if( href.startsWith('http') || href.startsWith('#') ) return;
-      link.setAttribute('href', `#${href}`)
+      link.setAttribute('href', `#!${href}`)
       link.addEventListener('click', () => this.loadPage(href))
     })
   }
@@ -41,12 +62,14 @@ class Flopumentation {
   loadPage = page => {
 
     this.page.toggleLoading(true)
+    this.page.current = page 
     this.page.element.main.classList.add('fade')
     
     fetch(page) 
       .then(response => response.text())
       .then(this.page.setContent)
       .finally(() => {
+        this.page.generateFooter(page)
         hljs.highlightAll()
         this.initContentListener()
         this.page.setContentAnchors()
--- src/scripts/Page.js
+++ src/scripts/Page.js
@@ -5,7 +5,22 @@ class Page {
   constructor(config) {
     this.config = config
     this.element = {}
+    this.pages = this.setPages()
   }
+
+  setPages = () => (
+    this.config.sections.reduce(
+      (pages, section) => ({
+        ...(pages), 
+        ...(section.pages.reduce(
+          (pages, page) => ({ 
+            ...pages, 
+            [page.content]: { ...page } 
+          }), {}
+        ))
+      }), {}
+    )
+  )
 
   /**
    * Generate the following html:
@@ -35,12 +50,13 @@ class Page {
       title      : document.createElement('h1'),
       menu       : document.createElement('nav'),
       main       : document.createElement('main'),
-      content    : document.createElement('div')
+      content    : document.createElement('div'),
+      footer     : document.createElement('footer')
     }
 
-    const ids = ['container', 'header', 'main', 'title', 'theme', 'content']
+    const ids = ['container', 'header', 'main', 'title', 'theme', 'content', 'footer']
     ids.forEach(name => {
-      this.element[ name ].id = name  
+      this.element[ name ].id = name
     })
 
     this.element.title.textContent = this.config.title ?? ''
@@ -49,6 +65,7 @@ class Page {
     this.element.header.className = 'load'
 
     this.element.main.appendChild(this.element.content)
+    this.element.main.appendChild(this.element.footer)
     this.element.header.appendChild(this.element.title)
     this.element.header.appendChild(this.element.theme)
     this.element.aside.appendChild(this.element.menu)
@@ -61,7 +78,19 @@ class Page {
 
     this.generateMenu()
   }
-
+  
+  /**
+   * <ul>
+   *   <li>
+   *     Secton Title
+   *     <ul>
+   *       <li><a href="#./path/to/page.md">Page name 1</a></li>
+   *       <li><a href="#./path/to/page.md">Page name 2</a></li>
+   *     </ul>
+   *   </li>
+   *   // ...
+   * </ul>
+   */
   generateMenu = () => {
 
     const sections = document.createElement('ul')
@@ -73,21 +102,6 @@ class Page {
 
     this.initMobileEvents()
     this.initThemeEvents()
-  }
-
-  initMobileEvents() {
-    this.element.aside.addEventListener('click', () => {
-      const isOpen = this.element.aside.getAttribute('data-open') !== 'true'
-      this.element.aside.setAttribute('data-open', isOpen ? 'true' : 'false')
-      if( isOpen ) this.element.menu.scrollTop = 0
-    })
-  }
-
-  initThemeEvents() {
-    this.element.theme.addEventListener('click', () => {
-      const theme = document.body.getAttribute('data-theme')
-      document.body.setAttribute('data-theme', theme === 'light' ? 'dark' : 'light')
-    })
   }
 
   addSection = section => {
@@ -111,10 +125,78 @@ class Page {
     const pageLink = document.createElement('a')
     
     pageLink.textContent = page.title
-    pageLink.href = `#${page.content}`
+    pageLink.href = `#!${page.content}`
     pageElement.appendChild(pageLink)
 
     return pageElement
+  }
+
+
+  /**
+   * <a href="./section-content/md">
+   *   <strong>Previous:</strong>  
+   *   <span>Section name</span>  
+   * </a>
+   * <a href="./section-content/md">
+   *   <strong>Next:</strong>  
+   *   <span>Section name</span>  
+   * </a>
+   */
+  generateFooter = page => {
+
+    this.element.footer.textContent = ''
+
+    const prev = this.getPreviousPage(page)
+    const next = this.getNextPage(page)
+
+    if( prev ) this.generateFooterNav(prev, 'previous')
+    if( next ) this.generateFooterNav(next, 'next')
+  }
+
+  generateFooterNav = (page, position) => {
+
+    const link = document.createElement('a')
+    link.id = `footer-nav-${position}`
+    link.href = page.content
+
+    const title = document.createElement('strong')
+    title.textContent = `${position === 'next' ? 'Next' : 'Previous'} page:`
+    link.appendChild(title)
+
+    const content = document.createElement('span')
+    content.textContent = page.title
+    link.appendChild(content)
+
+    this.element.footer.appendChild(link)
+  }
+
+  getPage = page => this.pages[page] ?? false
+  
+  getPreviousPage = page => {
+    const currentPosition = Object.keys(this.pages).indexOf(page)
+    const previousPage = Object.keys(this.pages)[ currentPosition - 1] ?? false
+    return this.getPage(previousPage) 
+  } 
+
+  getNextPage = page => {
+    const currentPosition = Object.keys(this.pages).indexOf(page)
+    const nextPage = Object.keys(this.pages)[ currentPosition + 1] ?? false
+    return this.getPage(nextPage) 
+  }
+
+  initMobileEvents() {
+    this.element.aside.addEventListener('click', () => {
+      const isOpen = this.element.aside.getAttribute('data-open') !== 'true'
+      this.element.aside.setAttribute('data-open', isOpen ? 'true' : 'false')
+      if( isOpen ) this.element.menu.scrollTop = 0
+    })
+  }
+
+  initThemeEvents() {
+    this.element.theme.addEventListener('click', () => {
+      const theme = document.body.getAttribute('data-theme')
+      document.body.setAttribute('data-theme', theme === 'light' ? 'dark' : 'light')
+    })
   }
 
   setContentAnchors = () => {
@@ -130,7 +212,7 @@ class Page {
   setContent = content => this.element.content.innerHTML = marked.parse(content) 
   getMenuItems = () => this.element.menu.querySelectorAll('nav a')
   getContentTitles = () => this.element.content.querySelectorAll('h1, h2, h3, h4, h5, h6')
-  getContentLinks = () => this.element.content.querySelectorAll('a')
+  getContentLinks = () => this.element.main.querySelectorAll('a')
   toggleLoading = loading => (
     this.element.loadingBar.setAttribute('data-loading', loading ? 'on' : 'off')
   )
